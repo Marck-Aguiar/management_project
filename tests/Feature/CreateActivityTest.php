c169f35e48cee3dfe854fbfec9c61481e362493f
Marck-Aguiar <cttmarckaguiar@gmail.com>
2024-12-23 00:33:56 -0300

    Added CreateActivityTest

---
+++ tests/Feature/CreateActivityTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Core\User\Models\User;
+use Core\Activity\DTOs\ActivityDTO;
+use Core\Activity\Cases\CreateActivity;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateActivityTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_it_can_create_activity(): void
+    {
+        $case = app(CreateActivity::class);
+
+        User::factory()->create();
+
+        $payload = new ActivityDTO(
+            description: "Ideia Inovadora",
+            points: 20,
+            userID: 1
+        );
+
+        $case->execute($payload);
+
+        $this->assertDatabaseCount('activities', 1);
+        $this->assertDatabaseHas('activities', [
+            'description' => 'Ideia Inovadora',
+            'points' => 20,
+            'user_id' => 1
+        ]);
+    }
+}
